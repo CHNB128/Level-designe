ee85f2eb7791231f9ebd42f043e04fac27c4d325
drake.aren <D.aren.nalson@gmail.com>
2018-04-25 17:03:54 +0600

    added toolbar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div id="tool-bar">
+    <button
+      class="btn btn-outline-secondary"
+      @click="$store.commit('showModal')">
+      load level
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  #tool-bar {
+    border-bottom: 1px solid;
+    padding: 0.25em;
+  }
+</style>
